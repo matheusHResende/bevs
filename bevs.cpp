8e6d770f8f2076727766c9b3f0f654ddfdd77269
jspagrow <jonassst@gmail.com>
2020-09-16 17:17:13 -0300

    update
    change bevs.cpp

--- bevs.cpp
+++ bevs.cpp
@@ -4,7 +4,7 @@
 #include "./src/Vertex.h"
 
 int main(){
-    Vertex* vertex = new Vertex("11 12");
+    Vertex* vertex = new Vertex("0 5");
     Vertex* vertex2 = new Vertex(0, 1);
     cout << "Dist: " << vertex->getDistanceTo(*vertex2) << endl;
     system("pause");
